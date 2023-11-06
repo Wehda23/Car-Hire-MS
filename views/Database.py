93d938873745591f91d928edf17f76d06186311b
Waheed Khaled <waheedkhaled95@gmail.com>
2023-11-06 03:58:30 +0200

    MySQLDatabase class underdevelopment in Database.py

--- views/Database.py
+++ views/Database.py
@@ -11,7 +11,7 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
-
+# Creates database in case does not exist Or Raise Error Database with this name does not exist
 class MySQLDatabase(mysql.connector):
     def __init__(
         self,
@@ -21,9 +21,36 @@ class MySQLDatabase(mysql.connector):
         user: str = MYSQL_USER,
         password: str = MYSQL_PASSWORD,
     ):
-        pass
+        self.dbname: str = dbname
+        self.host: str = host
+        self.port: int = port
+        self.user: str = user
+        self.password: str = password
+        self.mydb = None
 
+    def connect(self):
+        self.mydb = mysql.connector.connect(
+                host = self.host,
+                password = self.password,
+                port = self.port,
+                user = self.user,
+                database = self.dbname,
+                )
+        
+    def get_mysqldb(self):
+        if self.mydb:
+            return self.mydb
+        raise ValueError("self.mydb is undefined!.")
 
+    # method to create database
+
+    # method to close connection
+
+# TableCreation Class:
+    # Write class that checks if a table exists, If does not exist create table
+    # method to check_tables_exists
+
+    # method to create tables
 # Write a class to check the database
-# Creates database in case does not exist Or Raise Error Database with this name does not exist
-# Write class that checks if a table exists, If does not exist create table
+
+
