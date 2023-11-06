5c457fd5b130a8c606c5b88a97a93b0c60e94ab6
Waheed Khaled <waheedkhaled95@gmail.com>
2023-11-06 07:04:33 +0200

    Applying SOLID concepts on Database.py classes

--- test.py
+++ test.py
@@ -1,7 +1,15 @@
-from views.Database import MySQLDatabase
+from views.Database import MySQLConnection, MySQLManager
+from settings import DATBASE_NAME
 
+# Initiate MySQLConnection, It connects to MySQL but not the Database!
+mysql_connection: MySQLConnection = MySQLConnection()
 
+# Connect to mysql
+mysql_connection.connect()
 
-database: MySQLDatabase = MySQLDatabase()
+# Initiate MySQLManager
+mysql_manager: MySQLManager = MySQLManager(mysql_connection)
+# Create database
+mysql_manager.create_database(DATBASE_NAME)
 
-print(type(database))
+print(type(mysql_manager))
--- views/Database.py
+++ views/Database.py
@@ -1,18 +1,78 @@
+from abc import ABC, abstractmethod
 import mysql.connector
 from settings import MYSQL_USER, MYSQL_PASSWORD, DATBASE_NAME
 
 
-mydb = mysql.connector.connect(
-    host="localhost",
-    port=3306,
-    user=MYSQL_USER,
-    password=MYSQL_PASSWORD,
-)
+# Interface showing database connection
+class SQLConnection:
 
-mycursor = mydb.cursor()
+    def get_mysqldb(self):
+        pass
 
-# Creates database in case does not exist Or Raise Error Database with this name does not exist
-class MySQLDatabase:
+    def connect(self):
+        pass
+
+    def close(self):
+        pass
+
+
+# Connects to MySQL without Database
+class MySQLConnection(SQLConnection):
+
+    def __init__(
+        self,
+        host: str = "localhost",
+        port: int = 3306,
+        user: str = MYSQL_USER,
+        password: str = MYSQL_PASSWORD,
+    ):
+        self.host: str = host
+        self.port: int = port
+        self.user: str = user
+        self.password: str = password
+        self.mydb = None
+        self.mycursor = None
+
+    def get_mysqldb(self):
+        if self.mydb:
+            return self.mydb
+        raise ValueError("self.mydb is undefined!.")
+    
+    def connect(self) -> None:
+        """
+        Void Function used to connect to mysql
+        """
+        try:
+            # Check if self.mydb is already connected to a database or not to avoid Issues with resource management and Leaks
+            if self.mydb:
+                self.close()
+
+            self.mydb = mysql.connector.connect(
+                host=self.host,
+                password=self.password,
+                port=self.port,
+                user=self.user,
+            )
+
+            self.mycursor = self.mydb.cursor()
+
+        except mysql.connector.Error as e:
+            print(f"Error connecting to the mysql: {e}")
+    
+    def close(self) -> None:
+        # Check if the database is connected
+        if self.mydb.is_connected():
+            # Close Cursor first
+            self.mycursor.close()
+            # Close the mysql connection
+            self.mydb.close()
+            # Reset self.mydb to None
+            self.mydb = None
+            # Reset self.mydb to None
+            self.mycursor = None
+
+# Connects to MySQL Database directly
+class MySQLDatabaseConnection(SQLConnection):
     def __init__(
         self,
         dbname: str = DATBASE_NAME,
@@ -27,49 +87,98 @@ class MySQLDatabase:
         self.user: str = user
         self.password: str = password
         self.mydb = None
+        self.mycursor = None
 
-    def connect(self) -> None:
+
+    def connect(self, database_name: str = None) -> None:
         """
         Void Function used to connect to database
+
+        :database_name: Optional Parameter to provide a new database name incase needed (default: None)
         """
         try:
+            if not (isinstance(database_name, str)):
+                raise TypeError("parameter database_name should be of type <str>")
+
+            # If a new database name was entered it will overwrite the self.dbname variable other wise it is an optional parameter
+            if database_name:
+                self.dbname = database_name
+
+            # Check if self.mydb is already connected to a database or not to avoid Issues with resource management and Leaks
+            if self.mydb:
+                self.close()
+
             self.mydb = mysql.connector.connect(
-                    host = self.host,
-                    password = self.password,
-                    port = self.port,
-                    user = self.user,    
-                )
-            
+                host=self.host,
+                password=self.password,
+                port=self.port,
+                user=self.user,
+                database=self.dbname,
+            )
+
             self.mycursor = self.mydb.cursor()
 
         except mysql.connector.Error as e:
             print(f"Error connecting to the database: {e}")
-        
+
+        except TypeError as e:
+            print(e)
+
     def get_mysqldb(self):
         if self.mydb:
             return self.mydb
         raise ValueError("self.mydb is undefined!.")
 
-    # method to create database
-    def create_database(self) -> None:
+    def close(self) -> None:
+        # Check if the database is connected
+        if self.mydb.is_connected():
+            # Close Cursor first
+            self.mycursor.close()
+            # Close the mysql connection
+            self.mydb.close()
+            # Reset self.mydb to None
+            self.mydb = None
+            # Reset self.mydb to None
+            self.mycursor = None
+
+
+class MySQLManager:
+    def __init__(self, mysql_connection: MySQLConnection):
+        self.connection = mysql_connection
+    
+    def create_database(self, database_name: str) -> None:
         """
-        Method used to check if the database exists or not 
+        Method used to check if the database exists or not, Note function closes the connection after creating the database.
         """
         try:
-            pass
+            # Our query to get the database
+            query: str = "SHOW DATABASES LIKE %s"
+            # Values
+            values: tuple = (database_name,) # self.dbname
+            # Get database
+            print(type(self.connection))
+            self.connection.mycursor.execute(query,values)
+            result = self.connection.mycursor.fetchall()
+            
+            # Check if database exists
+            if result:
+                print(f"Database {database_name} Already Exists !")
+            else:
+                # Create the database
+                self.connection.mycursor.execute(f"CREATE DATABASE {database_name}")
+                print(f"Database '{database_name}' created.")
+            
+            self.connection.close()
+
         except mysql.connector.Error as e:
             print(f"Error creating/checking database: {e}")
-        
 
-
-    # method to close connection
 
 # TableCreation Class:
-    # Write class that checks if a table exists, If does not exist create table
-    # method to check_tables_exists
+# Write class that checks if a table exists, If does not exist create table
+# method to check_tables_exists
 
-    # method to create tables
+# method to create tables
 # Write a class to check the database
 
 # MySQLQuery Class:
-
